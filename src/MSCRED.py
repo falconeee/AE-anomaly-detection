9ef4cea9371d0877dc821624e750193965cdf388
Gabriel Falcone da Silva <falcone@MacBook-Air-de-Gabriel.local>
2026-04-13 21:36:18 -0300

    USAD implementation

--- src/MSCRED.py
+++ src/MSCRED.py
@@ -10,6 +10,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
+from scipy.optimize import minimize
 
 # Helper Classes (Models & Utils)
 def set_deterministic(seed=42):
